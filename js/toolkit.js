3f6f44ee910ecb53ab9ba6eba3f310d0de1346c5
hamzeen.hameem <Hamzeen.Hameem@sc.com>
2016-12-11 02:49:31 +0800

    added overlay and fixed scroll isssues

--- js/toolkit.js
+++ js/toolkit.js
@@ -1,8 +1,8 @@
+/* Sticky headers */
 var stickyHeaders = (function() {
   var $window = $(window),$stickies;
 
   var load = function(stickies) {
-    // console.log("onLoad");
     if (typeof stickies === "object" && stickies instanceof jQuery && stickies.length > 0) {
       $stickies = stickies.each(function() {
         var $thisSticky = $(this).wrap('<div class="followWrap" />');
@@ -13,15 +13,19 @@ var stickyHeaders = (function() {
               .height($thisSticky.outerHeight());
       });
 
+      // event listeners
       $window.off("scroll.stickies").on("scroll.stickies", function() {
         this.load;
-		      _whenScrolling();
+		    _whenScrolling();
+      });
+      $window.resize(function() {
+        this.load;
+        _whenScrolling();
       });
     }
   };
 
-  var _whenScrolling = function() {
-    // console.log($window.scrollTop());
+  var _whenScrolling = function() { // console.log($window.scrollTop());
     $stickies.each(function(i) {
       var $thisSticky = $(this), $stickyPosition = $thisSticky.data('originalPosition');
       if ($stickyPosition <= $window.scrollTop()) {
@@ -43,39 +47,76 @@ var stickyHeaders = (function() {
     });
   };
 
-  return {
-    load: load
-  };
-})();
+  return { load: load };
+})(); /*<!-- Sticky headers //-->*/
 
-$(document).ready(function() {
+function labs() {
   setTimeout(function() {
     $(".intro").addClass("show");
-  }, 900);
+  }, 800);
 
   setTimeout(function() {
     $(".secondary").addClass("show");
-  }, 1750);
+  }, 1400);
+}
+$(document).ready(function() {
+  labs();
 });
 
+/* anonymous function for triggering all other animations & compoents appearances */
 $(function() {
+
+  // image slides for 'Things To Do'
+  $(".things_to_do h1").click(function() {
+    var src = $(this).attr('ref');
+
+    /*$("#sg_slides img").animate({ "opacity": "0" }, "100" );
+    $("#sg_slides img").attr('src','slides/'+$(this).attr('ref'));
+    $("#sg_slides img").animate({ "opacity": "1" }, "500" );*/
+    $("#sg_slides img").fadeOut(function() {
+      $("#sg_slides img").fadeTo("0", .4);
+      $("#sg_slides img").attr('src','slides/' + src);
+      $("#sg_slides img").fadeTo("fast", 1.0);
+    });
+  });
+
+  // open overlay
+  $("#sg_btn_more").click(function() {
+    $(this).addClass("transform");
+    $("body").css({overflow: "hidden"});
+    $("#sg_overlay_bg").addClass("show");
+    $("#sg_overlay_wrap").addClass("show");
+    $(".overlay_close").addClass("open");
+  });
+
+  // close overlay
+  $("#sg_overlay_close").click(function() {
+    $("#sg_btn_more").removeClass("transform");
+    $("body").css({overflow: "auto"});
+    $("#sg_overlay_bg").removeClass("show");
+    $("#sg_overlay_wrap").removeClass("show");
+    $(this).removeClass("open");
+  });
+
   stickyHeaders.load($(".followMeBar"));
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     var winTop = $(window).scrollTop();
-    if(winTop >= ($(window).height()/3)) {
+    /*if(winTop >= ($(window).height()/3)) {
       $("body").addClass("sticky-header");
-      //$($(".followMeBar")[0]).addClass("fixed");
     } else{
       $("body").removeClass("sticky-header");
-    }
+    }*/ // earlier used for toggle scaling of intro text
 
     if(winTop >= $(window).height() - 20) {
       $(".slide-progress-container").addClass("show");
+      $(".intro").removeClass("show");
+      $(".secondary").removeClass("show");
     } else{
       $(".slide-progress-container").removeClass("show");
+      labs();
     }
-    //update progress transform: translate3d(50%, 0px, 0px);
 
+    //update progress transform: translate3d(50%, 0px, 0px);
     var scrollPercentage = 100 * (winTop - ($(window).height()+0)) / (3 * $(window).height());
     if(scrollPercentage<=100) {
       $(".slide-progress").css("transform", "translate3d(" + scrollPercentage + "%, 0px, 0px)");
